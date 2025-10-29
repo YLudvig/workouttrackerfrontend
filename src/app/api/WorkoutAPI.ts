daf3a4567b722a0f286f8f1778d5a7bcf23e644e
YLudvig <yslu24ne@student.ju.se>
2025-10-29 15:38:38 +0100

    close #67 close #64 close #54 close #53

--- src/app/api/WorkoutAPI.ts
+++ src/app/api/WorkoutAPI.ts
@@ -1,26 +1,26 @@
 import { environment } from "../../environments/environment";
-import { WorkoutTemplate } from "../types/Workoutsession";
+import { WorkoutList, WorkoutRequest } from "../types/Workoutsession";
 
 const token = localStorage.getItem("token");
 
 
-// Exporterar vår funktion för att skapa workouttemplate 
-export async function createTemplate(workoutTemplate: WorkoutTemplate): Promise<{data: string}> {
-    const response = await fetch(`${environment.backendApiUrl}/template/createTemplate`, {
+// Exporterar vår funktion för att skapa workout
+export async function createWorkout(workout: WorkoutRequest): Promise<{data: string}> {
+    const response = await fetch(`${environment.backendApiUrl}/workout/createWorkout`, {
         method : "POST", 
         headers: {
             "Content-Type": "application/json", 
             'Authorization': `Bearer ${token}`
         }, 
-        body: JSON.stringify({workoutTemplate})
+        body: JSON.stringify(workout)
     });
     const data = await response.text();
     return {data};
 }
 
-// Exporterar vår funktion för att skapa workouttemplate 
-export async function getAllTemplates(userId : number): Promise<WorkoutTemplate[]> {
-    const response = await fetch(`${environment.backendApiUrl}/template/getTemplates?userId=${userId}`, {
+// Exporterar vår funktion för att hämta workouts
+export async function getAllWorkouts(userId : number): Promise<WorkoutList[]> {
+    const response = await fetch(`${environment.backendApiUrl}/workout/getUsersWorkouts?userId=${userId}`, {
         method : "GET", 
         headers: {
             "Content-Type": "application/json", 
@@ -28,5 +28,6 @@ export async function getAllTemplates(userId : number): Promise<WorkoutTemplate[
         }, 
     });
     const result =  await response.json();
+    console.log(result);
     return result; 
 }
--- src/app/types/Workoutsession.ts
+++ src/app/types/Workoutsession.ts
@@ -1,6 +1,7 @@
 
 // Skapar typoer som motsvarar vad backend skickar för att typsäkra frontenden mer 
 export interface Exercise {
+    exerciseId: number; 
     exerciseName: string; 
     weight: number; 
     completed: boolean; 
@@ -8,15 +9,34 @@ export interface Exercise {
 
 export interface WorkoutSession {
     sessionId: string; 
-    templateId: number; 
-    templateName: string; 
+    workoutId: number; 
+    workoutName: string; 
     exercises: Exercise[];
     participantsIds: number[];
 }
 
-export interface WorkoutTemplate {
-    workoutTemplateId?: number; 
+export interface Workout {
+    workoutId: number; 
     userId : number; 
-    templateName: string; 
-    exercises: Exercise[];
+    workoutName: string; 
+    completed: boolean; 
+    completedAt: string | null; 
+}
+
+export interface WorkoutList {
+    exercises : Exercise[]; 
+    workout: Workout; 
+}
+
+export interface WorkoutRequest {
+    userId: number; 
+    workoutName: string; 
+    completed: boolean; 
+    exercises: ExerciseRequest[];
+}
+
+export interface ExerciseRequest {
+    exerciseName: string; 
+    weight: number; 
+    completed: boolean; 
 }
